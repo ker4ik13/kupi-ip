24195b2c24147cb8763bb90afdee395b7439211b
“DashaADM” <annettecoolley@gmail.com>
2025-01-30 04:22:37 +0300

    edit favicon

---
+++ src/app/manifest.ts
@@ -0,0 +1,35 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'KupiIP',
+    short_name: 'KupiIP',
+    description: 'VLESS ключи. Ваш ключ к безопасному интернету',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000013',
+    theme_color: '#000013',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: '48x48',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/favicon/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  }
+}
