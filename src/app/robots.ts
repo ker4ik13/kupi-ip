f90702c883cd64fca33c2f9565c10faa9e7ef88d
“DashaADM” <annettecoolley@gmail.com>
2025-04-21 13:09:37 +0300

    ignore etext param

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,11 +5,17 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = getServerSideURL()
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin', '/admin/', '/api', '/api/', '/guide/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/admin/', '/api', '/api/', '/guide/'],
+      },
+      {
+        userAgent: 'Yandex',
+        disallow: '\nClean-param: etext /',
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
